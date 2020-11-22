2ec364cdad158510307c8c22155f4e2e2b0467f2
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2020-11-22 14:43:17 +0300

    Add Kingfisher to project Complete Home Page

---
+++ Pods/Target Support Files/Pods-SpaceXLaunches/Pods-SpaceXLaunches-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpaceXLaunches : NSObject
+@end
+@implementation PodsDummy_Pods_SpaceXLaunches
+@end
---
+++ SpaceXLaunches/Utilities/Constants/UI.swift
@@ -0,0 +1,17 @@
+//
+//  UI.swift
+//  SpaceXLaunches
+//
+//  Created by Bugra's Mac on 21.11.2020.
+//
+
+import Foundation
+
+extension Constant {
+  
+  // MARK: - UI constants
+  
+  class UI {
+    static let homeTableViewCell = "HomeTableViewCell"
+  }
+}
